33fb71d727217e50e3271cfffe166388b0a266c4
Rahul Choudhary <rahul24167@gmail.com>
2024-12-17 20:47:12 +0530

    bootstraping done

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+//import rootRouter from './routes/index';
+//import { connectToDatabase } from './db/db';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use("/api/v1", rootRouter);
+
+//connectToDatabase().then(() => {
+  app.listen(10000, () => console.log(`Server running on port 3000`));
+//});
