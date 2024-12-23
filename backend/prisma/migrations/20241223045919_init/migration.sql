47d50f446540faa3e1427649bb27181f56ad7473
Rahul Choudhary <rahul24167@gmail.com>
2024-12-23 10:33:43 +0530

    schema created

---
+++ backend/prisma/migrations/20241223045919_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "photo" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "course" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "batch" TEXT NOT NULL,
+    "enroll" TEXT,
+    "phone" TEXT,
+    "linkdn" TEXT,
+    "twitter" TEXT,
+    "facebook" TEXT,
+    "instagram" TEXT,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "accountVerified" BOOLEAN NOT NULL DEFAULT false,
+    "phoneVarified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "photo" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "linkdn" TEXT,
+    "twitter" TEXT,
+    "facebook" TEXT,
+    "instagram" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
