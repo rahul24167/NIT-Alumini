0fc30d4a0640547d30add8217a9d19b66fca7166
Rahul Choudhary <rahul24167@gmail.com>
2025-01-08 20:47:59 +0530

    navigation logic to admin login added in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+  const navigate = useNavigate();
+  const handelClick = () => {
+    navigate("/adminsignin");
+  };
   return (
-    <div className="bg-blue-400">Footer
-    <div>Admin login</div></div>
-  )
-}
+    <div className="bg-blue-400">
+      Footer
+      <div>
+        <button onClick={handelClick}>Login as Admin</button>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
