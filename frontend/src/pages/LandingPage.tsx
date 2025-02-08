6a806bb67c33dad7bfcf84b44bc909ab344b024f
Rahul Choudhary <rahul24167@gmail.com>
2025-02-08 19:06:59 +0530

    issue in landing page is fixed

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 const LandingPage = () => {
   const closeGalleryModal = () => {
     const modal = document.getElementById("galleryModal");
