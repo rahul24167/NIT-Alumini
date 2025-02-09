0872210df4963cb458329e4b1adb0607874ea346
Rahul Choudhary <rahul24167@gmail.com>
2025-02-09 22:44:19 +0530

    upgraded HTTP to HTTPS

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://ec2-52-87-206-188.compute-1.amazonaws.com';
+export const BACKEND_URL = 'https://ec2-52-87-206-188.compute-1.amazonaws.com';
 export const FRONTEND_URL = 'http://localhost:5173';
